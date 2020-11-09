58a1020d817fc392a796d6ff603b864fd957a931
Rolando Martinez Gallardo <rolando.martinez@steinsvik.cl>
2020-11-09 16:50:12 -0300

    Add new EF Core Cliente ( console app)

---
+++ Complete/ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var context= new BloggingContext(connectionOptions()))
+            {
+                var count=context.Blogs.Count();
+                Console.WriteLine(count);
+
+            }
+            Console.ReadKey();
+        }
+
+
+        public static DbContextOptions<BloggingContext> connectionOptions()
+        {
+            var connection = "Server=PCRMARTINEZ\\SQL2016;Database=BlogEngineDb;Trusted_Connection=True;MultipleActiveResultSets=true";
+            var optionsBuilder = new DbContextOptionsBuilder<BloggingContext>();
+
+            optionsBuilder.UseSqlServer(connection);
+            return optionsBuilder.Options;
+
+        }
+
+
+    }
+}
